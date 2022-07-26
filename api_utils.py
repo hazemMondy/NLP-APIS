afb4a2b5a29bd9727dc17ae19f97fc4a60536964
hazemMondy <hm7734895@gmail.com>
2022-07-26 09:37:36 +0200

    V2.0.1

--- api_utils.py
+++ api_utils.py
@@ -1,6 +1,9 @@
+"""api_utils.py"""
+
 from typing import Optional, Dict, Tuple
+
 def dict_to_list(
-    dictionary:Dict[int,str])\
+    dictionary:Dict[int,str], cased: Optional[bool]=False)\
     ->Tuple[list,list]:
     """
     Convert dict to list
@@ -16,7 +19,13 @@ def dict_to_list(
         list of keys and values
         keys are ids
         values are text (answers)
+    
+    example:
+        >>> dict_to_list({1:"A",2:"B",3:"c"}, True)
+        ([1,2,3],["A","B","c"])
     """
     keys , vals = list(dictionary.keys()), list(dictionary.values())
+    if cased:
+        return keys, vals
     # convert every element to str
     return list(map(lambda x: str(x).lower(), keys)), list(map(lambda x: str(x).lower(), vals))
