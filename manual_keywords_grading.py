59ff684bca9e240caf6b13eb3ad871a65d2a38aa
hazemMondy <hm7734895@gmail.com>
2022-07-26 09:40:55 +0200

    V2.0.1

--- manual_keywords_grading.py
+++ manual_keywords_grading.py
@@ -15,7 +15,11 @@ MEDIUM_ENCLOSURE = cfg['enclosure_medium']
 SOFT_ENCLOSURE = cfg['enclosure_soft']
 HARD_ENCLOSURE = cfg['enclosure_hard']
 DEFAULT_WEIGHT = cfg['default_weight']
+DEFAULT_EMPTY_WEIGHT = cfg['default_empty_weight']
 
+if not DEBUGGING:
+    import warnings
+    warnings.filterwarnings('ignore')
 class ManualKeywordsGradingModel(object):
     """ManualKeywordsGradingModel"""
     # words_emb_dict = {}
@@ -47,8 +51,8 @@ class ManualKeywordsGradingModel(object):
             np.array of shape (768,) or (384,) depending on the encoder model
 
         example:
-            >>> kgm = KeywordsGradingModel(BERTModel)
-            >>> kgm._get_word_emb('hello')
+            >>> mkgm = KeywordsGradingModel(BERTModel)
+            >>> mkgm._get_word_emb('hello')
             array([0.01, 0.02, 0.03, ..., 0.99, 0.98, 0.97])
         """
         if word in self.words_emb_dict:
@@ -68,6 +72,12 @@ class ManualKeywordsGradingModel(object):
 
         Returns:
             List[str]: list of candidates words/phrases
+
+        example:
+            >>> mkgm = ManualKeywordsGradingModel(BERTModel)
+            >>> doc = 'You need to know how much vinegar was used in each container.'
+            >>> mkgm.__candidates_tokens(doc)
+            ['know vinegar used', 'vinegar used container', 'need know vinegar']
         """
         try:
             count = CountVectorizer(
@@ -92,10 +102,10 @@ class ManualKeywordsGradingModel(object):
             List of candidates.
 
         example:
-            >>> kgm = KeywordsGradingModel(BERTModel)
+            >>> mkgm = ManualKeywordsGradingModel(BERTModel)
             >>> n_grams = [(1,2),(2,3)]
             >>> doc = 'You need to know how much vinegar was used in each container.'
-            >>> kgm._get_candidates(n_grams,doc)
+            >>> mkgm._get_candidates(n_grams,doc)
             ['know vinegar used', 'vinegar used container', 'need know vinegar']
         """
         return flatten(list(map(lambda gram :
@@ -111,9 +121,9 @@ class ManualKeywordsGradingModel(object):
             np.array of shape (docs_number,768) or (docs_number,384) depending on the encoder model
 
         example:
-            >>> kgm = KeywordsGradingModel(BERTModel)
+            >>> mkgm = ManualKeywordsGradingModel(BERTModel)
             >>> keywords = ['hello','world']
-            >>> kgm.emb_keywords_(keywords)
+            >>> mkgm.emb_keywords_(keywords)
             array([[0.01, 0.02, 0.03, ..., 0.99, 0.98, 0.97],
                    [0.01, 0.02, 0.03, ..., 0.99, 0.98, 0.97]])
         """
@@ -134,10 +144,10 @@ class ManualKeywordsGradingModel(object):
             List[List[str]]: list of top_n keyprhases
 
         example:
-            >>> kgm = KeywordsGradingModel(BERTModel)
+            >>> mkgm = ManualKeywordsGradingModel(BERTModel)
             >>> docs = ['You need to know how much vinegar was used in each container.']
             >>> keywords = {'vinegar','container'}
-            >>> kgm.keywords_exrtaction(docs,keywords)
+            >>> mkgm.keywords_exrtaction(docs,keywords)
             array([[0.789, 0.987, 0.32484 ....]])  (not real example)
         """
         if isinstance(docs, str):
@@ -186,12 +196,12 @@ class ManualKeywordsGradingModel(object):
                 each keyword for each document
 
         example:
-            >>> kgm = KeywordsGradingModel(BERTModel)
+            >>> mkgm = ManualKeywordsGradingModel(BERTModel)
             >>> docs_keywords = [['know vinegar used', 'vinegar used container',
                  'need know vinegar']]
             >>> model_answer_keywords = [['know vinegar used', 'vinegar used container',
                  'need know vinegar']]
-            >>> kgm.keywords_grading(docs_keywords,model_answer_keywords)
+            >>> mkgm.keywords_grading(docs_keywords,model_answer_keywords)
             array([[[1.0000002 , 1.0000002 , 0.99999964]]], dtype=float32)
         """
         if grading_type.lower() == "hard":
@@ -202,7 +212,7 @@ class ManualKeywordsGradingModel(object):
             threshold = SOFT_THRESHOLD
         if grading_type.lower() == "medium":
             threshold = MEDIUM_THRESHOLD
-        # ! BATCH
+
         embs = self.model.encode(manual_keywords)
         if DEBUGGING:
             list(map(lambda docs_cand :print(("before",embs.shape, docs_cand.shape))
@@ -221,55 +231,88 @@ class ManualKeywordsGradingModel(object):
 
     def keywords_grading_predict(self:object, hard_grades:np.ndarray,
         medium_grades:np.ndarray, soft_grades:np.ndarray,
-        hard_weights:List[float], medium_weights:List[float], soft_weights:List[float])->np.ndarray:
+        hard_weights:List[float], medium_weights:List[float], soft_weights:List[float],
+        n_essays:int)->np.ndarray:
         """
         Args:
-            grades (np.ndarray): array of shape (docs_number,top_n)
-            containing the scores for each keyword for each document
+            hard_grades (np.ndarray): hard grades for each document
+            medium_grades (np.ndarray): medium grades for each document
+            soft_grades (np.ndarray): soft grades for each document
+            hard_weights (List[float]): weights for hard grading
+            medium_weights (List[float]): weights for medium grading
+            soft_weights (List[float]): weights for soft grading
+            n_essays (int): number of essays to grade
 
         Returns:
-            np.array of shape (n_model_answers,docs_number) containing
-                the scores for each keyword for each document
+            np.array of shape (docs_number,) containing the final scores
 
         example:
-            >>> kgm = KeywordsGradingModel(BERTModel)
+            >>> mkgm = ManualKeywordsGradingModel(BERTModel)
             >>> grades = np.array([[[0.761, 0.242, 0.263, 0.832, 0.437,
                 0.259, 0.269, 0.314, 0.218,0.237],
                 [0.509, 0.392, 0.472, 0.451, 0.924, 0.366, 0.24 , 0.314, 0.236,
                 0.237]]], dtype=float32)
-            >>> kgm.keywords_grading_predict(grades)
+            >>> mkgm.keywords_grading_predict(grades)
             array([[0.467, 0.401]])
         """
         weights_dict = { "hard": hard_weights, "medium": medium_weights, "soft": soft_weights }
         grades_dict = { "hard": hard_grades, "medium": medium_grades, "soft": soft_grades }
-        for _, grade_weight in weights_dict.items():
-            if len(grade_weight) == 0:
-            # if grade_weight is None:
-                grade_weight = np.array([0])
+        weights, grades = 0, 0
+        for keyword_type, grade_weight in weights_dict.items():
+            if grade_weight[0] == DEFAULT_EMPTY_WEIGHT:
+                continue
+            elif len(grade_weight) == 0:
+                grade_weight = np.array([DEFAULT_WEIGHT])
             elif grade_weight[0] == " ":
-                grade_weight = np.array([0])
-        weights = np.concatenate(list(weights_dict.values()), axis=0)
+                grade_weight = np.array([DEFAULT_WEIGHT])
+            if isinstance(weights, int):
+                weights = np.array(grade_weight)
+                grades = grades_dict[keyword_type]
+            else:
+                weights = np.concatenate((weights, grade_weight))
+                grades = np.concatenate((grades, grades_dict[keyword_type]))
+
         if DEBUGGING:
             for _, grade in grades_dict.items():
                 print("shape", grade.shape)
-        grades = np.vstack(grades_dict.values())
-        if DEBUGGING:
             print("grades shape", grades.shape)
             print("weights shape", weights.shape)
             print("weights", weights)
+
+        if isinstance(weights, int):
+            if DEBUGGING:
+                print("no keywords available")
+                print("weights", weights)
+            return np.zeros((n_essays,)).ravel()
+
         # fill weights nan with mean
         # get weights > 0 to ignore empty weights
-        weights = np.nan_to_num(weights, nan=np.nanmean(weights[weights > 0.0]))
+        means = weights[weights > DEFAULT_WEIGHT]
+        if means.size == 0:
+            means = np.array([1])
+        weights = np.nan_to_num(weights, nan=np.nanmean(means))
+
         if DEBUGGING:
             print("weights", weights)
-
         if weights.shape[0] == 0:
             if DEBUGGING:
                 print(grades.shape)
             return grades.mean(axis=0)
-        # weights = np.array(list(map(lambda x: weights_dict[x], ["hard", "medium", "soft"])))
-        # return np.array(list(map(lambda x: np.sum(x*weights, axis=0),
-        #     [hard_grades, medium_grades, soft_grades]))).astype(float).round(3)
+        # * instructor forgot to add weights to the grades
+        if not weights.any():
+            if DEBUGGING:
+                print("weights all 0", weights, grades.mean(axis=0).shape, grades.shape)
+            grades = 0
+            for _, grade in grades_dict.items():
+                if grade.any():
+                    if isinstance(grades,int):
+                        if DEBUGGING:
+                            print("Grade is 0")
+                        grades = grade
+                    else:
+                        grades = np.concatenate((grades, grade))
+            return grades.mean(axis=0)
+
         return np.average(grades, axis=0, weights=weights).astype(float).round(3).clip(0,1)
 
     def pipeline(self:object, answers:List[str], enclosure:str,
@@ -290,19 +333,19 @@ class ManualKeywordsGradingModel(object):
             List[float]: list of weights for each keyword
 
         example:
-            >>> kgm = KeywordsGradingModel(BERTModel)
+            >>> mkgm = ManualKeywordsGradingModel(BERTModel)
             >>> answers = ["know \"\"vinegar\"\"0.8 used", "vinegar used container",
                 "need know the size"]
             >>> enclosure = "\"\""
             >>> grading_type = "hard"
-            >>> kgm.pipeline(answers, enclosure, grading_type)
+            >>> mkgm.pipeline(answers, enclosure, grading_type)
             ('know used', array([1.0 , 1.0 , 0.0]), [DEFAULT_WEIGHT])
         """
         keywords = get_str_between(answers[0], enclosure)
         if DEBUGGING:
             print(keywords)
         grades = np.zeros(len(answers[1:]))
-        keywords_weights = [0]
+        keywords_weights = [DEFAULT_EMPTY_WEIGHT]
         if len(keywords) != 0:
             if DEBUGGING:
                 print("keywords:", keywords)
@@ -314,6 +357,7 @@ class ManualKeywordsGradingModel(object):
                 manual_keywords = keywords, answers = answers[1:], grading_type = grading_type)
             # clean model answer
             answers[0] = clean_doc(answers[0], keywords, keywords_weights, enclosure)
+
         return answers[0], grades, keywords_weights
 
     def predict(
@@ -330,7 +374,11 @@ class ManualKeywordsGradingModel(object):
             Dict[str, float]: dictionary of ids with their scores
 
         example:
-            >>> kgm.predict(answers,ids)
+            >>> mkgm = ManualKeywordsGradingModel(BERTModel)
+            >>> answers = ["know \"\"vinegar\"\"0.8 used", "vinegar used container",
+                "need know the size"]
+            >>> ids = list(range(len(answers)))
+            >>> mkgm.predict(answers,ids)
             {'id1': 0.8, 'id2': 0.7}
         """
         if answers is None or ids is None:
@@ -352,5 +400,6 @@ class ManualKeywordsGradingModel(object):
         grades = self.keywords_grading_predict(
             hard_grades = keywords_hard_grades, medium_grades = keywords_medium_grades,
             soft_grades = keywords_soft_grades, hard_weights = keywords_hard_weights,
-            medium_weights = keywords_medium_weights, soft_weights = keywords_soft_weights)
+            medium_weights = keywords_medium_weights, soft_weights = keywords_soft_weights,
+            n_essays=len(answers[1:]))
         return zip(ids[1:], grades)
