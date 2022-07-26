2e20f6982f278a07c7de034fccabe8ed01e23889
hazemMondy <hm7734895@gmail.com>
2022-07-26 09:40:40 +0200

    V2.0.1

--- configs.py
+++ configs.py
@@ -9,10 +9,12 @@ ENCLOSURE_MEDIUM = "\"\"@@"
 ENCLOSURE_HARD = "\"\""
 DEBUGGING = False
 DEFAULT_WEIGHT = np.nan
+DEFAULT_EMPTY_WEIGHT = np.NINF
 EXCEPTION_ENTITES = set(["DATE","TIME","PERCENT","MONEY","QUANTITY","ORDINAL", "CARDINAL"])
 NER_PATH = 'en_core_web_sm'
-SIAMESE_NER_MODEL_PATH = 'models/model_rg.pkl.pickle'
+SIAMESE_NER_MODEL_PATH = 'models/model_rg.pickle'
 KWRDS_MODEL_PATH = 'models/kwrds_model.pickle'
+ENCODER_PATH = "sentence-transformers/paraphrase-multilingual-MiniLM-L12-v2"
 
 configs = {
     'debugging': DEBUGGING,
@@ -24,8 +26,27 @@ configs = {
     'threshold_soft': THRESHOLD_SOFT,
     'threshold_medium': THRESHOLD_MEDIUM,
     'default_weight': DEFAULT_WEIGHT,
+    'default_empty_weight': DEFAULT_EMPTY_WEIGHT,
     'exception_entities': EXCEPTION_ENTITES,
     'ner_path': NER_PATH,
     'siamese_ner_model_path': SIAMESE_NER_MODEL_PATH,
-    'kwrds_model_path': KWRDS_MODEL_PATH
+    'encoder_path': ENCODER_PATH
+    }
+# keywords_grading.py
+DEFAULT_PADDING_LENGTH = 10 # coefficient of the neural network's input length
+DEFAULT_PADDING_VALUE = ""
+TOP_N_KEYWORDS = 10 # number of keywords to extract from an essay
+DIVERSITY = 0.7
+N_GRAMS = [(2,3)]
+BATCH_SIZE = 5
+
+configs_keywords = {
+    'debugging': DEBUGGING,
+    'kwrds_model_path': KWRDS_MODEL_PATH,
+    'padding_length': DEFAULT_PADDING_LENGTH,
+    'padding_value': DEFAULT_PADDING_VALUE,
+    'top_n_keywords': TOP_N_KEYWORDS,
+    'diversity': DIVERSITY,
+    'n_grams': N_GRAMS,
+    'batch_size': BATCH_SIZE
     }
