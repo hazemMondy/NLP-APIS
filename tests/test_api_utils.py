dc6a9bd06f1f872808ad6692f4c5fdb856f31e7d
hazemMondy <hm7734895@gmail.com>
2022-07-30 11:30:25 +0200

    ADD: testing api utils

---
+++ tests/test_api_utils.py
@@ -0,0 +1,26 @@
+import api_utils
+import pytest
+
+class TestApiutilsDicttolist:
+    def test_dict_to_list_happy(self):
+        param1 = { "one": "Good answer", "id2v": "bad ANswer" }
+        result = api_utils.dict_to_list(param1, False)
+        assert result == (["one", "id2v"], ["good answer", "bad answer"]), str(result)
+
+    def test_dict_to_list_cased(self):
+        result = api_utils.dict_to_list({ "one": "Good answer", "id2v": "bad ANswer" }, True)
+        assert result == (["one", "id2v"], ["Good answer", "bad ANswer"])
+
+    def test_dict_to_list_integers(self):
+        result = api_utils.dict_to_list({ 1: "good answer", 2: "bad answer" })
+        assert result == (["1", "2"], ["good answer", "bad answer"])
+
+    def test_dict_to_list_empty(self):
+        result = api_utils.dict_to_list({})
+        assert isinstance(result, tuple), "output is not a tuple"
+        assert len(result) == 2, "output is not a tuple of length 2"
+        assert result == ([], []), "output is not empty"
+
+    def test_dict_to_list_all_floats(self):
+        result = api_utils.dict_to_list({ 1: 60.0, 2.0: 55.5 })
+        assert result == (["1", "2.0"], ["60.0", "55.5"]), "output is not strings"
