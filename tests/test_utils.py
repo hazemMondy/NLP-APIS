199e4320c9229ae7b1a703c64ac6f35ff9954675
hazemMondy <hm7734895@gmail.com>
2022-07-31 10:18:47 +0200

    ADD: utils testing

---
+++ tests/test_utils.py
@@ -0,0 +1,38 @@
+import pytest
+import utils
+
+class TestUtilsFlatten(object):
+    _message = lambda args: f"actual error message is: '{args[0]}' should be: '{args[1]}'"
+
+    def test_flatten_none(self):
+        actual = utils.flatten([])
+        assert actual == [], self._message((actual, []))
+
+    def test_flatten_happy(self):
+        actual = utils.flatten([12, ["s0175"], 56784, [56784, 98750, "a1969"]])
+        expected = [12, "s0175", 56784, 56784, 98750, "a1969"]
+        assert actual == expected, self._message((actual, expected))
+
+    def test_flatten_already_flat(self):
+        actual = utils.flatten([12, "s0175", 56784, 56784, 98750, "a1969"])
+        expected = [12, "s0175", 56784, 56784, 98750, "a1969"]
+        assert actual == expected, self._message((actual, expected))
+
+    def test_flatten_cubed_nested_lists(self):
+        degree = None
+        actual = utils.flatten([12, [["s0175"]], 56784, [56784, 98750, "a1969"]],degree)
+        expected = [12, ["s0175"], 56784, 56784, 98750, "a1969"]
+        assert actual == expected, self._message((actual, expected))
+
+        degree = 3
+        actual = utils.flatten([12, [["s0175"]], 56784, [56784, 98750, "a1969"]],degree)
+        expected = [12, "s0175", 56784, 56784, 98750, "a1969"]
+        assert actual == expected, self._message((actual, expected))
+
+    def test_flatten_non_lists(self):
+        for arg in [12, "asdfjhh", 5678.4,{"hi":123}]:
+            with pytest.raises(TypeError) as exc_info:
+                utils.flatten(arg)
+        expected_error_msg = "input must be a list"
+        message = f"actual error message is: '{exc_info.value}' should be: '{expected_error_msg}'"
+        assert exc_info.match(expected_error_msg), message
